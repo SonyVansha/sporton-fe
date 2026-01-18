e120fcaa05a37b1021427252af1771b1d9e88dfb
SonyVansha <sonyvansha@gmail.com>
2026-01-18 20:34:27 +0700

    Live Session 1 - Pengenalan React.js & Slicing Home Page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from "./components/home/hero";
+import CategoriesSection from "./components/home/categories";
+import ProductsSection from "./components/home/products";
+
+export default function Home() {
+  return ( 
+    <main>
+      <HeroSection/>
+
+      <CategoriesSection/>
+
+      <ProductsSection />
+    </main>
+  )
+}
